ca21b29f6ee09121bd8471b22dc0a096e9253aa3
kmangutov <kmangutov@gmail.com>
2015-12-18 11:40:22 -0600

    upload progres

--- js/behavior.js
+++ js/behavior.js
@@ -1,7 +1,7 @@
 
 
 var canvasWidth = 450;
-var circleRadius = 15;
+var circleRadius = 10;
 var imgUrl = "https://m2.behance.net/rendition/pm/23685659/max_1200/be1c0f3d2bfb8c5f414129e768bf8b64.jpg"; //"http://i.imgur.com/9mwuTql.jpg"
 
 var canvasHandle;
@@ -9,26 +9,104 @@ var context2d;
 
 var arrayFeedbacks = [];
 
-var renderFeedbackVisuals = function() {
-  context2d.clearRect(0, 0, canvasHandle.width, canvasHandle.height);
+var canvasOffsetX = 0;
+var canvasOffsetY = 0;
+
+var tempFeedback = {active: false};
+
+var renderFloatingInput = function(evt) {
+  $("#floating-input").show();
+  $("#floating-input").css({left: evt.pageX, top: evt.pageY + 5});
+}
+
+var renderFloatingDisplay = function(feedback) {
+  $("#floating-display").show();
+  var newX = canvasOffsetX + feedback.x;
+  var newY = canvasOffsetY + feedback.y;
+  $("#floating-display").css({left: newX, top: newY});
+  $("#floating-display").val(feedback.text);
+}
+
+var dist = function(x1, y1, x2, y2) {
+  return Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
+}
+
+var resetActive = function() {
   for(var i in arrayFeedbacks) {
     var feedback = arrayFeedbacks[i];
-    console.log(JSON.stringify(feedback));
-    context2d.fillStyle = "#FFF";
-    context2d.strokeStyle = "#000";
-    context2d.beginPath();
-    context2d.arc(feedback.x, feedback.y, circleRadius, 0, 2 * Math.PI);
-    context2d.fill();
-    context2d.stroke();
-
-    context2d.fillStyle = "#000";
-    context2d.font = "18pt Arial";
-    context2d.beginPath();
-    context2d.fillText(parseInt(i) + 1, feedback.x - circleRadius/2, feedback.y + circleRadius/2);
+    feedback.active = false;
   }
+}
+
+var checkMouseOver = function(loc) {
+
+  for(var i in arrayFeedbacks) {
+    var feedback = arrayFeedbacks[i];
+    if(!feedback.active && dist(loc.x, loc.y, feedback.x, feedback.y) <= circleRadius) {
+      console.log("mouseoveredf one");
+
+      if(!feedback.hover) {
+        renderFloatingDisplay(feedback);
+        feedback.hover = true;
+      }
+    } else {
+      if(feedback.hover) {
+        $("#floating-display").hide();
+        feedback.hover = false;
+      }
+    }
+  }
+}
+
+var onLeaveInput = function() {
+  var val = $("#floating-input").val();
+
+  if(val.length > 0 && tempFeedback.active) {
+    tempFeedback.text = $("#floating-input").val();
+    arrayFeedbacks.push(tempFeedback);////
+  }
+  
+  tempFeedback = {active: false};
+  $("#floating-input").val("");
+}
+
+var doMouseDown = function(evt) {
+
+  onLeaveInput();
+
+  resetActive();
+  evt.stopPropagation();
+  tempFeedback = {
+    id: arrayFeedbacks.length + 1,
+    x: evt.pageX - canvasOffsetX,
+    y: evt.pageY - canvasOffsetY,
+    absX: evt.pageX,
+    absY: evt.pageY,
+    text: "hello world",
+    hover: false,
+    active: true
+  };
+
+ 
+  renderFeedbackVisuals();
+  renderFloatingInput(evt);
+}
+
+var renderFeedbackVisuals = function() {
+
+  context2d.clearRect(0, 0, canvasHandle.width, canvasHandle.height);
+  for(var i in arrayFeedbacks) {
+
+    var feedback = arrayFeedbacks[i];
+    renderKnob(context2d, feedback);  
+  }
+  renderKnob(context2d, tempFeedback);//
 };
 
 $(document).ready(function(){
+
+  $("#floating-display").hide();
+  $("#floating-input").hide();
 
   canvasHandle = document.getElementById("canvas");
   var designHandle = document.getElementById("imgDesign");
@@ -36,24 +114,31 @@ $(document).ready(function(){
 
   context2d = canvasHandle.getContext("2d");;
 
-  designHandle.onload = function() {
+  $("#imgDesign").imagesLoaded(function() {
+    console.log("designHandle onload width: " + designHandle.width);
     canvasHandle.width = designHandle.width;
     canvasHandle.height = designHandle.height;
-  };
+  });
 
-  canvasHandle.addEventListener("mousedown", function(evt) {
+
+
+  canvas.addEventListener("mousemove", function(evt) {
+    
     var offset = $(this).offset();
-    evt.stopPropagation();
-    console.log(JSON.stringify(offset));
+
+    canvasOffsetX = offset.left;
+    canvasOffsetY = offset.top;
+
     var obj = {
-      id: arrayFeedbacks.length + 1,
       x: evt.pageX - offset.left,
       y: evt.pageY - offset.top,
-      text: "hello world"
     };
 
-    arrayFeedbacks.push(obj);
-    renderFeedbackVisuals();
+    checkMouseOver(obj);
+  });
+
+  canvasHandle.addEventListener("mousedown", function(evt) {
+    doMouseDown(evt);
   }, false);
 }); 
 
